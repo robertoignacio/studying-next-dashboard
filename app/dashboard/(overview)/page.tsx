9d5c865c173759dbecfef9de26dd0a9cd9ea83cc
robertoignacio <rvs.git@gmail.com>
2024-01-29 21:34:26 -0300

    enabled fetchLatestInvoices() streaming

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,17 +5,28 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 
-// To fetch data for the <RevenueChart/> component, import the fetchRevenue function from data.ts
-// etc
-import { fetchRevenue } from '@/app/lib/data';
-import { fetchLatestInvoices } from '@/app/lib/data';
+// moved fetchrevenue() to a Suspense component
+// import { fetchRevenue } from '@/app/lib/data';
+// moved fetchLatestInvoices() to a Suspense component;
+//import { fetchLatestInvoices } from '@/app/lib/data';
 import { fetchCardData } from '@/app/lib/data'; 
+
+// import <Suspense> from React, to wrap it around <RevenueChart /> and <LatestInvoices />
+import { Suspense } from 'react';
+// You can pass it fallback components
+import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { LatestInvoicesSkeleton } from '@/app/ui/skeletons';
+
 
 // Page is an async component, which allows you to use await to fetch data
 export default async function Page() {
 
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
+  // removed
+  // const revenue = await fetchRevenue();
+
+  // removed 
+  // const latestInvoices = await fetchLatestInvoices();
+
   // destructure the data returned from fetchCardData
   const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData();
 
@@ -46,8 +57,13 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        {/* remove the props */}
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
